f96f537affa5c4f9978109c28de4217d38400b42
TaddyRiv <131029997+TaddyRiv@users.noreply.github.com>
2025-09-29 23:28:41 -0400

    sc

--- src/shared/api.js
+++ src/shared/api.js
@@ -6,7 +6,7 @@ const api = axios.create({ baseURL: API_BASE });
 
 // Interceptor para agregar siempre el access_token
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token"); // 👈 aquí
+  const token = localStorage.getItem("access_token"); 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
