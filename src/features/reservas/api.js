c4fb91ef7bada1f7fcb74e328ebe473ce1fd9aee
TaddyRiv <131029997+TaddyRiv@users.noreply.github.com>
2025-09-30 01:02:53 -0400

    revicion

--- src/features/reservas/api.js
+++ src/features/reservas/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "https://3.17.18.25/api";
+const API_BASE = process.env.REACT_APP_API_URL || "https://3.17.18.25/api";
 
 
 const authHeader = () => ({
